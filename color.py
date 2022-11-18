4347d79850d2aae61060d5d8795fec4e76b34b89
QuentinPerret <quentinprrt33@gmail.com>
2022-11-18 01:12:43 +0100

    teint almost finished

--- color.py
+++ color.py
@@ -1,6 +1,11 @@
 import sys
 import tkinter as tk
 import matplotlib.colors as col #for hsv to rgb
+import numpy as np 
+
+wordList = ["maison","tête","ville","temps","porte" ,"pays","route","raison",
+"homme","cœur","femme","dieu","amour","monde" ,"voiture" ,"jour",
+"temps","monsieur","bien","personne","fois" ,"part","rue" ,"chambre","monde"]
 
 class ColorWin():
     
@@ -18,13 +23,10 @@ class ColorWin():
         self.iTeintBleu = 0
         self.range = 5
 
-        self.window.bind("<F11>", self.toggleFullScreen)
-        self.window.bind("<Escape>", self.quitFullScreen)
-        self.window.bind("<space>", self.nextColor)
-
-        self.window.configure(bg=self._from_rgb(255, 255, 0)) 
+        self.firstPage()
 
         self.window.mainloop()
+        self.f.close()
 
     def toggleFullScreen(self , event):
         self.fullScreenState = not self.fullScreenState
@@ -35,15 +37,50 @@ class ColorWin():
         self.window.attributes("-fullscreen", self.fullScreenState)
 
     def nextColor(self , event):
+        self.clearWin()
         if(255 -self.range*self.iTeintRouge>0):
             self.iTeintRouge += 1
-            self.window.configure(bg=self._from_rgb(255 -self.range*self.iTeintRouge, 255, 0)) 
+            self.bg = self._from_rgb(255 -self.range*self.iTeintRouge, 255, 0)
         else:
             self.iTeintBleu += 1
-            self.window.configure(bg=self._from_rgb(0, 255,self.range*self.iTeintBleu)) 
-        print(self.range*self.iTeintRouge,self.range*self.iTeintBleu )
+            self.bg=self._from_rgb(0, 255,self.range*self.iTeintBleu)
+        
+        self.window.configure(bg=self.bg) 
+        self.greenWord = tk.Label(self.window, text=wordList[np.random.randint(len(wordList))], bg = self.bg, fg = self._from_rgb(0,255,0))
+        self.greenWord.place(relx = np.random.randint(10,91)/100 , rely = np.random.randint(10,91)/100 , anchor = 'center')
+
 
     def _from_rgb(self,r,g,b):
         return f'#{r:02x}{g:02x}{b:02x}'
+
+    def firstPage(self):
+        tk.Label(self.window, text='Enter The Subject Name & Hit Enter Key').pack(pady=20)
+        self.name_Tf = tk.Entry(self.window)
+        self.name_Tf.bind('<Return>',self.createNewFileAndCleanWin)
+        self.name_Tf.pack()
+
+    def clearWin(self):
+        for widget in self.window.winfo_children():
+            widget.destroy()
+
+    def teintTest(self):
+        self.window.bind("<F11>", self.toggleFullScreen)
+        self.window.bind("<Escape>", self.quitFullScreen)
+        self.window.bind("<space>", self.nextColor)
+        self.window.bind("<Return>", self.writeData)
+
+        self.window.configure(bg=self._from_rgb(255, 255, 0))
+
+    def createNewFileAndCleanWin(self,name):
+        name = self.name_Tf.get()
+        fileName = name + ".txt"
+        self.clearWin()
+        self.f = open(fileName,'a')
+        self.teintTest()
+    
+    def writeData(self , event):
+        print(str(255 -self.range*self.iTeintRouge) +",255,"+ str(self.range*self.iTeintBleu))
+        self.f.write(str(255 -self.range*self.iTeintRouge) +",255,"+ str(self.range*self.iTeintBleu)+"\n")
+
 
 ColorWin()
